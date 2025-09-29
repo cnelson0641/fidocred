d38af86995fe6c8f8b7aafc6ce1a717299bbebba
Chris Nelson <cnelson0641@gmail.com>
2025-09-29 16:12:17 -0600

    Update

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,4 +1,8 @@
-variable "env_name" {
+variable "gitlab_env" {
     description = "Dev, test, or PROD environment."
     type        = string
+    validation {
+        condition = contains(["dev", "test", "PROD", var.gitlab_env)
+        error_message = "gitlab_env must be: dev, test, PROD"
+    }
 }

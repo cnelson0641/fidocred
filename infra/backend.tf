f4fefb59a2d6e597b28f770615b2ad531021a7c9
Chris Nelson <cnelson0641@gmail.com>
2025-10-02 15:57:24 -0600

    Update

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,3 +1,9 @@
 terraform {
-  backend "gitlab" {}
+    backend "s3" {
+        bucket = "fidocred-dev-tfstate"
+        key = "terraform.tfstate"
+        region = "us-east-1"
+        dynamodb_table = fidocred-dev-locks"
+        encrypt = true
+    }
 }

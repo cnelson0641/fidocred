75d78e00cb134e6211e741b3d25a79079c577ae6
Chris Nelson <cnelson0641@gmail.com>
2025-10-03 10:41:28 -0600

    Updated tf to call call api correctly

--- infra/main.tf
+++ infra/main.tf
@@ -58,5 +58,5 @@ resource "aws_lambda_permission" "apigw_permission" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.fastapi_lambda.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/${aws_apigatewayv2_stage.stage.name}/*/*"
 }

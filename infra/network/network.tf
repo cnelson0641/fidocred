bdb4f9ff14a864d4f60d91ad93771ee0ac1e73d4
Chris Nelson <cnelson0641@gmail.com>
2025-10-17 15:49:05 -0600

    restructured into 3 tf states and applies.

--- infra/network/network.tf
+++ infra/network/network.tf
@@ -13,14 +13,12 @@ resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.fidocred_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = var.availability_zone
 }
 
 # Private Subnet for DB
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.fidocred_vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = var.availability_zone
 }
 
 # Internet Gateway for Lambda egress
